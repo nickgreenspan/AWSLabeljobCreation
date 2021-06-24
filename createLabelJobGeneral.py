d4b3e9d816cdcf49ceb8feb2fcad289799a1db66
Ubuntu <ubuntu@ip-172-31-26-204.ec2.internal>
2021-06-24 20:26:28 +0000

    fixed bugs when reading from frames

--- createLabelJobGeneral.py
+++ createLabelJobGeneral.py
@@ -6,14 +6,14 @@ import yaml
 from preprocess import preprocess_job
 
 if __name__ == "__main__":
-	video_bucket = sys.argv[1] #$bucketname
-	video_name = sys.argv[2] #$dataname
-	video_path = sys.argv[3] #$inputpath
-	config_name = sys.argv[4] #$configname
-	config_path = sys.argv[5] #$configpath and assume custom config file layout
-	input_data_bucket = video_bucket #assuming they should be the same
-	output_dir = sys.argv[6] #$processdir
-	lab_group_name = sys.argv[7] #$groupdir
+        video_bucket = sys.argv[1] #$bucketname
+        video_name = sys.argv[2] #$dataname
+        video_path = sys.argv[3] #$inputpath
+        config_name = sys.argv[4] #$configname
+        config_path = sys.argv[5] #$configpath and assume custom config file layout
+        input_data_bucket = video_bucket #assuming they should be the same
+        output_dir = sys.argv[6] #$processdir
+        lab_group_name = sys.argv[7] #$groupdir
 
 
 #testing purposes ###############################
@@ -26,22 +26,22 @@ s3 = boto3.resource('s3', region_name = 'us-east-1')
 print("video_bucket:" +  video_bucket)
 s3.Bucket(video_bucket).download_file(config_path, config_name)
 task_labels = []
-with open('config.yaml', 'r') as f:
-		doc = yaml.load(f)
-		users = doc['users']
-		annotationtype = doc['anntype']
-		jobname = doc['jobname']
-		datasetname = doc['datasetname'] #note requirements for datasetname
-		shortintruct = doc['shortintruct']
-		fullinstruct = doc['fullinstruct']
-		bodyparts = doc['bodyparts']
-		target_bucket = doc['finaldatabucket']
-		skeleton = doc['skeleton']
-		data_format = doc['dataformat']
-		for label in bodyparts:
-			task_labels.append({'label': label})
-		numframes = doc['numframes2pick']
-
+print(config_name)
+with open(config_name, 'r') as f:
+                doc = yaml.load(f)
+                users = doc['users']
+                annotationtype = doc['anntype']
+                jobname = doc['jobname']
+                datasetname = doc['datasetname'] #note requirements for datasetname
+                shortintruct = doc['shortintruct']
+                fullinstruct = doc['fullinstruct']
+                bodyparts = doc['bodyparts']
+                target_bucket = doc['finaldatabucket']
+                skeleton = doc['skeleton']
+                data_format = doc['dataformat']
+                for label in bodyparts:
+                        task_labels.append({'label': label})
+                numframes = doc['numframes2pick']
 os.remove(config_name)
 model_config = {
 'process_dir': output_dir, #added as lambda function needs this info to get the output of the labeling job
@@ -78,79 +78,77 @@ model_config = {
 'y1': 277,
 'y2': 624}
 with open('config.yaml', 'w') as f: #creating dlc config file
-	yaml.dump(model_config, f)
+        yaml.dump(model_config, f)
 
 s3.Bucket(target_bucket).upload_file('config.yaml', jobname + '/data/config.yaml')
+os.remove('config.yaml') #deleting model training config file
 upid = 'us-east-1_ZxGaQUSI2'
 clientId = '7cujg3m8o3sh6cqg39lcbc1ool'
 #labeluri = 'https://pgvx2rzogw.labeling.us-east-1.sagemaker.aws'
 
 def createLabelJob(users, jobname, input_data_bucket, datasetname, anntype):   #You must use the same clientId for all of your workteams, so all workteams must be part of the same userpool
-	try:
-		group = client.create_group(GroupName = lab_group_name, UserPoolId = upid) #note groupname requirements
-	except:
-		group = client.get_group(GroupName = lab_group_name, UserPoolId = upid)
-	
-	for user in users:
-		try: 
-			userinfo = client.admin_create_user(UserPoolId=upid, Username = user[0], UserAttributes=[{'Name': 'email', 'Value': user[1]}]) #once the users are added to the userpool they don't need to be added again
-		except:
-			print(user[0] + " already in userpool, continuing")
-		
-		try:
-			client.admin_add_user_to_group(UserPoolId = upid, Username = user[0], GroupName = group['Group']['GroupName'])
-		except:
-			print(user[0] + " already in user group, continuing")
+        try:
+                group = client.create_group(GroupName = lab_group_name, UserPoolId = upid) #note groupname requirements
+        except:
+                group = client.get_group(GroupName = lab_group_name, UserPoolId = upid)
+        
+        for user in users:
+                try: 
+                        userinfo = client.admin_create_user(UserPoolId=upid, Username = user[0], UserAttributes=[{'Name': 'email', 'Value': user[1]}]) #once the users are added to the userpool they don't need to be added again
+                except:
+                        print(user[0] + " already in userpool, continuing")
+                
+                try:
+                        client.admin_add_user_to_group(UserPoolId = upid, Username = user[0], GroupName = group['Group']['GroupName'])
+                except:
+                        print(user[0] + " already in user group, continuing")
 
-	#You cannot create more than 25 work teams in an account and region
-	try: 
-		workteam = smclient.create_workteam(WorkteamName= "Team" + lab_group_name, MemberDefinitions=[{'CognitoMemberDefinition': {'UserPool': upid, 'UserGroup': group['Group']['GroupName'], 'ClientId': clientId}}], Description='Team' + lab_group_name)
-		workteam = smclient.describe_workteam(WorkteamName = "Team" + lab_group_name)
-	except:
-		workteam = smclient.describe_workteam(WorkteamName = "Team" + lab_group_name)
-	
-	humantaskuiarn = ""
-	prehumantasklambdaarn = ""
-	annotationconsolidationconfigarn = ""
-	if (anntype == "Keypoint"):
-		humantaskuiarn = 'arn:aws:sagemaker:us-east-1:394669845002:human-task-ui/VideoObjectTracking'
-		prehumantasklambdaarn = 'arn:aws:lambda:us-east-1:432418664414:function:PRE-VideoObjectTracking'
-		annotationconsolidationconfigarn = 'arn:aws:lambda:us-east-1:432418664414:function:ACS-VideoObjectTracking'
+        #You cannot create more than 25 work teams in an account and region
+        try: 
+                workteam = smclient.create_workteam(WorkteamName= "Team" + lab_group_name, MemberDefinitions=[{'CognitoMemberDefinition': {'UserPool': upid, 'UserGroup': group['Group']['GroupName'], 'ClientId': clientId}}], Description='Team' + lab_group_name)
+                workteam = smclient.describe_workteam(WorkteamName = "Team" + lab_group_name)
+        except:
+                workteam = smclient.describe_workteam(WorkteamName = "Team" + lab_group_name)
+        
+        humantaskuiarn = ""
+        prehumantasklambdaarn = ""
+        annotationconsolidationconfigarn = ""
+        if (anntype == "Keypoint"):
+                humantaskuiarn = 'arn:aws:sagemaker:us-east-1:394669845002:human-task-ui/VideoObjectTracking'
+                prehumantasklambdaarn = 'arn:aws:lambda:us-east-1:432418664414:function:PRE-VideoObjectTracking'
+                annotationconsolidationconfigarn = 'arn:aws:lambda:us-east-1:432418664414:function:ACS-VideoObjectTracking'
 
-	job = smclient.create_labeling_job(LabelingJobName= jobname, LabelAttributeName= "Label-ref", #could change labelattributename
-		InputConfig={'DataSource': {'S3DataSource': {'ManifestS3Uri' : "s3://"+ input_data_bucket + "/" + lab_group_name + "/inputs/" + datasetname + ".manifest.json"}}}, 
-		OutputConfig= {'S3OutputPath': ("s3://"+ input_data_bucket + "/" + lab_group_name + "/" + output_dir)}, 
-		RoleArn = "arn:aws:iam::739988523141:role/labeljobcreator", #predefined role
-		LabelCategoryConfigS3Uri = "s3://"+ input_data_bucket + "/" + lab_group_name + "/inputs/label_config_full.json",
-		HumanTaskConfig = {
-			'WorkteamArn': workteam['Workteam']['WorkteamArn'],
-			'UiConfig': {'HumanTaskUiArn': humantaskuiarn}, #need to be changed depending on label job type
-	        'PreHumanTaskLambdaArn': prehumantasklambdaarn,
-	        'TaskTitle': (jobname),
-	        'TaskDescription': "Label the data", #can change if necessary
-	        'NumberOfHumanWorkersPerDataObject': 1, #can change this
-	        'TaskTimeLimitInSeconds' : 172800,
-	        'AnnotationConsolidationConfig': {
-	            'AnnotationConsolidationLambdaArn': annotationconsolidationconfigarn #we should figure what we want to do about conflicting annotations
-	        }
-	    })
+        job = smclient.create_labeling_job(LabelingJobName= jobname, LabelAttributeName= "Label-ref", #could change labelattributename
+                InputConfig={'DataSource': {'S3DataSource': {'ManifestS3Uri' : "s3://"+ input_data_bucket + "/" + lab_group_name + "/inputs/" + datasetname + ".manifest.json"}}}, 
+                OutputConfig= {'S3OutputPath': ("s3://"+ input_data_bucket + "/" + lab_group_name + "/" + output_dir)}, 
+                RoleArn = "arn:aws:iam::739988523141:role/labeljobcreator", #predefined role
+                LabelCategoryConfigS3Uri = "s3://"+ input_data_bucket + "/" + lab_group_name + "/inputs/label_config_full.json",
+                HumanTaskConfig = {
+                        'WorkteamArn': workteam['Workteam']['WorkteamArn'],
+                        'UiConfig': {'HumanTaskUiArn': humantaskuiarn}, #need to be changed depending on label job type
+                'PreHumanTaskLambdaArn': prehumantasklambdaarn,
+                'TaskTitle': (jobname),
+                'TaskDescription': "Label the data", #can change if necessary
+                'NumberOfHumanWorkersPerDataObject': 1, #can change this
+                'TaskTimeLimitInSeconds' : 172800,
+                'AnnotationConsolidationConfig': {
+                    'AnnotationConsolidationLambdaArn': annotationconsolidationconfigarn #we should figure what we want to do about conflicting annotations
+                }
+            })
 
-	#workteam_info = smclient.describe_workteam(WorkteamName = "Team" + lab_group_name)
-	labeluri = workteam['Workteam']['SubDomain']
+        #workteam_info = smclient.describe_workteam(WorkteamName = "Team" + lab_group_name)
+        labeluri = workteam['Workteam']['SubDomain']
 
-	#writes labeling url info to file and uploads to bucket
-	file1 = open('labeling_urls.txt', 'w')
-	file1.write("https://neurocaas.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=" + clientId + "&redirect_uri=https://"+ labeluri + "/oauth2/idpresponse\n")
-	file1.write(labeluri)
-	file1.close()
-	s3.Bucket(input_data_bucket).upload_file('labeling_urls.txt', lab_group_name + '/' + output_dir +  '/labeling_urls.txt')
-
-	print("https://neurocaasdomain.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=" + clientId + "&redirect_uri=https://"+ labeluri + "/oauth2/idpresponse")
-	print("labeluri: " + labeluri)
+        #writes labeling url info to file and uploads to bucket
+        file1 = open('labeling_urls.txt', 'w')
+        file1.write("https://neurocaas.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=" + clientId + "&redirect_uri=https://"+ labeluri + "/oauth2/idpresponse\n")
+        file1.write(labeluri)
+        file1.close()
+        s3.Bucket(input_data_bucket).upload_file('labeling_urls.txt', lab_group_name + '/' + output_dir +  '/labeling_urls.txt')
+        os.remove('labeling_urls.txt')
+        print("https://neurocaasdomain.auth.us-east-1.amazoncognito.com/login?response_type=code&client_id=" + clientId + "&redirect_uri=https://"+ labeluri + "/oauth2/idpresponse")
+        print("labeluri: " + labeluri)
 
 preprocess_job(data_format, jobname, video_bucket, video_path, video_name, input_data_bucket, target_bucket, lab_group_name, numframes, annotationtype, task_labels, datasetname, shortintruct, fullinstruct)
 createLabelJob(users, jobname, input_data_bucket, datasetname, annotationtype)
-
-os.remove(config_name)
-os.remove('labeling_urls.txt')
 
--- preprocess.py
+++ preprocess.py
@@ -11,81 +11,104 @@ import zipfile
 s3client = boto3.client('s3', region_name = 'us-east-1')
 s3 = boto3.resource('s3', region_name = 'us-east-1')
 
+def sorter(x):
+    x = x.split("/")[-1]
+    x = x.split(".")[0]
+    num_idx = -1
+    for i in range(len(x)):
+        if ((x[i].isnumeric())):
+            num_idx = i
+            break
+    if (num_idx == -1):
+        return 0
+    try:
+        return int(x[num_idx:])
+    except:
+        return 0
 def preprocess_job(data_format, job_name, video_bucket, video_path, video_name, input_data_bucket, target_bucket, lab_group_name, numframes, anntype, labels, datasetname, shortintruct, fullinstruct):
-	#newbuc = s3.create_bucket(ACL = 'public-read-write', Bucket=input_data_bucket) #if bucket doesn't already exist
-	if (data_format == "frames"):
-		folder_base = video_name.split('.', 1)[0] #gets the name of the zipfile without the extention
-		s3.Bucket(video_bucket).download_file(video_path, video_name) #downloads zip file of folder of frames
-		unzippedfile = zipfile.ZipFile(video_name, "r")
-		frames = []
-		f = 0
-		sequence_1 = {}
-		sequence_1["seq-no"] = 1
-		sequence_1["prefix"] = ("s3://" + input_data_bucket + "/" + lab_group_name + '/inputs/' + video_base + "/")
-		file_names = unzippedfile.namelist()
-		for name in file_names:
-			if name == ".DS_Store":
-				continue
-			s3client.upload_file(Bucket = input_data_bucket, Key = (lab_group_name + '/inputs/' + video_base + "/" + name), Filename = name)
-			frame_dict = {}
-			frame_dict["frame-no"] = f + 1
-			frame_dict["unix-timestamp"] = 2 #doesn't matter
-			frame_dict["frame"] = (name)
-			frames.append(frame_dict)
-			f += 1
-		sequence_1["frames"] = frames
-		sequence_1["number-of-frames"] = f		
+        #newbuc = s3.create_bucket(ACL = 'public-read-write', Bucket=input_data_bucket) #if bucket doesn't already exist
+        if (data_format == "frames"):
+                video_base = video_name.split('.', 1)[0] #gets the name of the zipfile without the extention
+                s3.Bucket(video_bucket).download_file(video_path, video_name) #downloads zip file of folder of frames
+                unzippedfile = zipfile.ZipFile(video_name, "r")
+                frames = []
+                f = 0
+                sequence_1 = {}
+                sequence_1["seq-no"] = 1
+                sequence_1["prefix"] = ("s3://" + input_data_bucket + "/" + lab_group_name + '/inputs/' + video_base + "/")
+                file_names = unzippedfile.namelist()
+                file_names_sorted = sorted(file_names, key = sorter)
+                for name in file_names_sorted:
+                    name_start = name.split("/")[0]
+                    if name_start != video_base:
+                        continue
+                    file_name = name.split("/")[1]
+                    if (len(file_name) == 0):
+                        continue
+                    extension = file_name.split(".")[1]
+                    if (extension != "png" and extension != "jpeg" and extension != "jpg"): #think about other extensions
+                        continue
+                    print(file_name)
+                    unzippedfile.extract(name)
+                    s3client.upload_file(Bucket = input_data_bucket, Key = (lab_group_name + '/inputs/' + video_base + "/" + file_name), Filename = name)
+                    os.remove(name)
+                    frame_dict = {}
+                    frame_dict["frame-no"] = f + 1
+                    frame_dict["unix-timestamp"] = 2 #doesn't matter
+                    frame_dict["frame"] = file_name
+                    frames.append(frame_dict)
+                    f += 1
+                sequence_1["frames"] = frames
+                sequence_1["number-of-frames"] = f              
 
-	else: #data_format == "video"
-		video_base = video_name.split('.', 1)[0] #gets the filename without extension
-		s3.Bucket(video_bucket).download_file(video_path, video_name)
-		s3client.copy_object(Bucket = target_bucket, CopySource = {"Bucket" : video_bucket, "Key": video_path}, Key = job_name + "/data/videos/"+ video_name) #copies the original video to the output location
-		cap = cv2.VideoCapture(video_name)
-		frameRate = cap.get(5)
-		frames = []
-		f = 0
-		sequence_1 = {} #starting with only one sequence
-		sequence_1["seq-no"] = 1
-		sequence_1["prefix"] = ("s3://" + input_data_bucket + "/" + lab_group_name + '/inputs/' + video_base + "/")
+        else: #data_format == "video"
+                video_base = video_name.split('.', 1)[0] #gets the filename without extension
+                s3.Bucket(video_bucket).download_file(video_path, video_name)
+                s3client.copy_object(Bucket = target_bucket, CopySource = {"Bucket" : video_bucket, "Key": video_path}, Key = job_name + "/data/videos/"+ video_name) #copies the original video to the output location
+                cap = cv2.VideoCapture(video_name)
+                frameRate = cap.get(5)
+                frames = []
+                f = 0
+                sequence_1 = {} #starting with only one sequence
+                sequence_1["seq-no"] = 1
+                sequence_1["prefix"] = ("s3://" + input_data_bucket + "/" + lab_group_name + '/inputs/' + video_base + "/")
+                while(cap.isOpened()):
+                        if (f >= numframes):
+                                print("exceeded")
+                                break
+                        frameId = cap.get(1) #current frame number
+                        ret, frame = cap.read()
+                        if (ret != True):
+                            break
+                        hasFrame, imageBytes = cv2.imencode(".jpg", frame)
+                        if(hasFrame):
+                                s3client.put_object(Bucket= input_data_bucket, Key=(lab_group_name + '/inputs/' + video_base + '/frame_' + str(f) + '.jpeg'), Body=imageBytes.tobytes())
+                                frame_dict = {}
+                                frame_dict["frame-no"] = f + 1
+                                frame_dict["unix-timestamp"] = 2 #doesn't matter
+                                frame_dict["frame"] = ("frame_" + str(f) + '.jpeg')
+                                frames.append(frame_dict)
+                                f += 1          
+                cap.release()
+                sequence_1["frames"] = frames
+                sequence_1["number-of-frames"] = f + 1
 
-		while(cap.isOpened()):
-			if (f >= numframes):
-				print("exceeded")
-				break
-			frameId = cap.get(1) #current frame number
-			ret, frame = cap.read()
-			if (ret != True):
-			    break
-			hasFrame, imageBytes = cv2.imencode(".jpg", frame)
-			if(hasFrame):
-				s3client.put_object(Bucket= input_data_bucket, Key=(lab_group_name + '/inputs/' + video_base + '/frame_' + str(f) + '.jpeg'), Body=imageBytes.tobytes())
-				frame_dict = {}
-				frame_dict["frame-no"] = f + 1
-				frame_dict["unix-timestamp"] = 2 #doesn't matter
-				frame_dict["frame"] = ("frame_" + str(f) + '.jpeg')
-				frames.append(frame_dict)
-				f += 1		
-		cap.release()
+        #uploads sequence file    
+        s3client.put_object(Bucket = input_data_bucket, Key=(lab_group_name + '/inputs/' + video_base + '/seq1.json'), Body=(bytes(json.dumps(sequence_1).encode('UTF-8'))))
 
-		sequence_1["frames"] = frames
-		sequence_1["number-of-frames"] = f + 1
+        #creates manifest file
+        manifest = {}
+        manifest["source-ref"] = "s3://" + input_data_bucket + '/' + lab_group_name + '/inputs/' + video_base + "/seq1.json"
 
-	#uploads sequence file    
-	s3client.put_object(Bucket = input_data_bucket, Key=(lab_group_name + '/inputs/' + video_base + '/seq1.json'), Body=(bytes(json.dumps(sequence_1).encode('UTF-8'))))
+        s3client.put_object(Bucket = input_data_bucket, Key=(lab_group_name + '/inputs/' + datasetname + '.manifest.json'), Body=(bytes(json.dumps(manifest).encode('UTF-8'))))
 
-	#creates manifest file
-	manifest = {}
-	manifest["source-ref"] = "s3://" + input_data_bucket + '/' + lab_group_name + '/inputs/' + video_base + "/seq1.json"
+        #create label config file
+        label_config = {}
+        label_config["document-version"]= datetime.today().strftime('%Y-%m-%d')
+        label_config["annotationType"] = anntype #ex: "Keypoint" 
+        label_config["instructions"] = {"shortInstruction": shortintruct, "fullInstruction": fullinstruct}
+        label_config["labels"] = labels
 
-	s3client.put_object(Bucket = input_data_bucket, Key=(lab_group_name + '/inputs/' + datasetname + '.manifest.json'), Body=(bytes(json.dumps(manifest).encode('UTF-8'))))
-
-	#create label config file
-	label_config = {}
-	label_config["document-version"]= datetime.today().strftime('%Y-%m-%d')
-	label_config["annotationType"] = anntype #ex: "Keypoint" 
-	label_config["instructions"] = {"shortInstruction": shortintruct, "fullInstruction": fullinstruct}
-	label_config["labels"] = labels
-
-	s3client.put_object(Bucket = input_data_bucket, Key=(lab_group_name + '/inputs/label_config_full.json'), Body=(bytes(json.dumps(label_config).encode('UTF-8'))))
-	os.remove(video_name)
+        s3client.put_object(Bucket = input_data_bucket, Key=(lab_group_name + '/inputs/label_config_full.json'), Body=(bytes(json.dumps(label_config).encode('UTF-8'))))
+        os.remove(video_name)
 
